63617e2f584bb9052912ae7708ab5a24e8b0d9fa
ubed pathan <ubedpathan818@gmail.com>
2024-12-15 19:57:08 +0530

    Todo_Server

--- src/main/java/com/ubedpathan/TodoApp/TodoAppApplication.java
+++ src/main/java/com/ubedpathan/TodoApp/TodoAppApplication.java
@@ -8,8 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodoAppApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		
 		SpringApplication.run(TodoAppApplication.class, args);
 	}
 
